44298727a5a77814b49f144df21e14bcc2207adb
yd <kyd5022@naver.com>
2022-06-03 16:22:50 +0900

    test1

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,9 @@ from .backbones import *  # noqa
 from .builder import (build_backbone, build_head, build_loss, build_neck,
                       build_posenet)
 
+import sys 
+sys.path.append('models')
+
 __all__ = [
     'build_backbone', 'build_head',
     'build_loss', 'build_posenet', 'build_neck'
